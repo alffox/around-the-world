49f1dbb1a9b66f824a7795fd249374503fafa08a
alffox <alfonso.crisci.it@gmail.com>
2018-08-28 12:33:37 +0200

    feat: Add newsAPI.org ajax service

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-alert('ciao');
+$.ajax({
+  url:
+    `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${newsAPIKey}`,
+  method: "GET",
+  error: function() {
+    console.log("there was an error");
+  },
+  success: function(data) {
+    processData(data);
+  }
+});
+
+function processData(data) {
+  var articleItems = [];
+
+  for (var i = 0; i < data.articles.length; i++) {
+    var author = data.articles[i].author;
+    var title = data.articles[i].title;
+    var description = data.articles[i].description;
+    var artUrl = data.articles[i].url;
+
+    var $author = $('<div class="author">Author: ' + author + "</div >");
+    var $title = $(
+      "<a href=" + artUrl + '><div class="title">' + title + "</div ></a>"
+    );
+    var $description = $(
+      "<a href=" +
+        artUrl +
+        '><div class="description">' +
+        description +
+        "</div ></a>"
+    );
+
+    $(".wrapper").append($author, $title, $description);
+    console.log(artUrl);
+  }
+}
