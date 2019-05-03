b5e7694a6bdbc1a3c3bb2f5939057a6538c110bf
alffox <alfonso.crisci.it@gmail.com>
2019-05-03 14:17:10 +0200

    chore: Switch port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(morgan('combined'));
 
