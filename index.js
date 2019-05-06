d340756d0de64a65a9fa29e92895e4fc52d4bcea
alffox <alfonso.crisci.it@gmail.com>
2019-05-06 10:55:08 +0200

    chore: Use Heroku port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000
 
 app.use(morgan('combined'));
 
