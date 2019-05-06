2fc0a64098b8b3fbf96769bdd432f9ce0b5c331f
alffox <alfonso.crisci.it@gmail.com>
2019-05-06 10:57:03 +0200

    chore: Use Heroku port variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(morgan('combined'));
 
